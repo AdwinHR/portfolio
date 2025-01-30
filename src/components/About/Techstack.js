5582c999996f8057dc9d40d6be8f66a7c635953a
AdwinHR <84682276+AdwinHR@users.noreply.github.com>
2025-01-31 00:30:13 +0530

    commit

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+  DiJava,
+} from "react-icons/di";
+import {
+  SiRedis,
+  SiFirebase,
+  SiNextdotjs,
+  SiSolidity,
+  SiPostgresql,
+} from "react-icons/si";
+import { TbBrandGolang } from "react-icons/tb";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus title="C++" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 title="JavaScript" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <TbBrandGolang title="Golang" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs title="Node.js" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact title="React" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSolidity title="Solidity" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb title="MongoDB" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs title="Next.js" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit title="Git" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase title="Firebase" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRedis title="Redis" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql title="PostgreSQL" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython title="Python" />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava title="Java" />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Col, Row, OverlayTrigger, Tooltip } from "react-bootstrap";
+import {
+  SiMacos,
+  SiVisualstudiocode,
+  SiPostman,
+  SiSlack,
+  SiVercel,
+  SiMicrosoftteams,
+  SiMicrosoftazure,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <OverlayTrigger
+          placement="top"
+          overlay={<Tooltip id="tooltip-macos">macOS</Tooltip>}
+        >
+          <span>
+            <SiMacos />
+          </span>
+        </OverlayTrigger>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <OverlayTrigger
+          placement="top"
+          overlay={<Tooltip id="tooltip-vscode">Visual Studio Code</Tooltip>}
+        >
+          <span>
+            <SiVisualstudiocode />
+          </span>
+        </OverlayTrigger>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <OverlayTrigger
+          placement="top"
+          overlay={<Tooltip id="tooltip-postman">Postman</Tooltip>}
+        >
+          <span>
+            <SiPostman />
+          </span>
+        </OverlayTrigger>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <OverlayTrigger
+          placement="top"
+          overlay={<Tooltip id="tooltip-teams">Microsoft Teams</Tooltip>}
+        >
+          <span>
+            <SiMicrosoftteams />
+          </span>
+        </OverlayTrigger>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <OverlayTrigger
+          placement="top"
+          overlay={<Tooltip id="tooltip-vercel">Azure</Tooltip>}
+        >
+          <span>
+            <SiMicrosoftazure />
+          </span>
+        </OverlayTrigger>
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
